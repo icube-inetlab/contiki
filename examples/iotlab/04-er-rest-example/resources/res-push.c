d6d4f0dd1fda7303a552109a86691bee951d12a6
Guillaume Schreiner <schreiner@unistra.fr>
2021-06-01 17:35:44 +0200

    increase periodic message interval to 60s for push resource

--- examples/iotlab/04-er-rest-example/resources/res-push.c
+++ examples/iotlab/04-er-rest-example/resources/res-push.c
@@ -49,7 +49,7 @@ PERIODIC_RESOURCE(res_push,
                   NULL,
                   NULL,
                   NULL,
-                  5 * CLOCK_SECOND,
+                  60 * CLOCK_SECOND,
                   res_periodic_handler);
 
 /*
